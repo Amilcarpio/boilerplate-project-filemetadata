5bb333c0b2cb5b84292daea9eaae45df60872a2b
Amilcar Pio <amilcar.pio@gmail.com>
2022-08-05 18:30:14 -0300

    projeto-upload

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 var app = express();
 
 app.use(cors());
@@ -11,10 +12,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
